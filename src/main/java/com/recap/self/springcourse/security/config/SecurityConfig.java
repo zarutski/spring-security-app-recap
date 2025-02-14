b6817726c6d6902a3cc3885906c2dffbcab8cb82
Maksim Zarutski <zarutski93maksim@gmail.com>
2025-02-14 14:18:41 +0300

    simple authentication (spring default implementation)

--- src/main/java/com/recap/self/springcourse/security/config/SecurityConfig.java
+++ src/main/java/com/recap/self/springcourse/security/config/SecurityConfig.java
@@ -1,24 +1,31 @@
 package com.recap.self.springcourse.security.config;
 
-import com.recap.self.springcourse.security.secure.AuthProviderImpl;
+import com.recap.self.springcourse.security.service.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     // authentication config
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
     }
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
